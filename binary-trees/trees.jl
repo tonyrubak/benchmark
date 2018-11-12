27297e38ede77122d5b00a7e7a0307cec7f3e7c2
tonyrubak <joe@crimsonhexagon.us>
2018-11-12 10:50:49 -0500

    refactored worker functions into a separate file

--- binary-trees/trees.jl
+++ binary-trees/trees.jl
@@ -1,65 +1,8 @@
 using Distributed
-@everywhere abstract type AbstractTree end
 
-@everywhere struct EmptyTree <: AbstractTree end
+@everywhere include("tree_worker.jl")
 
-@everywhere struct TreeNode <: AbstractTree
-    left::AbstractTree
-    right::AbstractTree
-end
-
-@everywhere function create_tree(depth)
-    if depth == 0
-        TreeNode(EmptyTree(), EmptyTree())
-    else
-        depth = depth - 1
-        TreeNode(create_tree(depth), create_tree(depth))
-    end
-end
-
-@everywhere function check_tree(tree)
-    if typeof(tree.left) === EmptyTree
-        1
-    else
-        1 + check_tree(tree.left) + check_tree(tree.right)
-    end
-end
-
-@everywhere function make_check(itde)
-    i, depth = itde
-    check_tree(create_tree(depth))
-end
-
-@everywhere function get_argchunks(i, d, chunksize = 5000)
-    @assert chunksize % 2 == 0
-    num_chunks = i % chunksize == 0 ? div(i, chunksize) :
-        div(i, chunksize) + 1
-    chunks = Array{Array{Tuple{Int64,Int64}}}(undef, num_chunks)
-    for j in 1:num_chunks-1
-        chunk = Array{Tuple{Int64, Int64}}(undef, chunksize)
-        for k in 1:chunksize
-            @inbounds chunk[k] = (k + chunksize*(j-1), d)
-        end
-        chunks[j] = chunk
-    end
-        
-    chunk = Array{Tuple{Int64, Int64}}(undef, i % chunksize)
-    for j in 1:(i % chunksize)
-        @inbounds chunk[j] = (j + chunksize*(num_chunks-1), d)
-    end
-    chunks[num_chunks] = chunk
-    chunks
-end
-
-@everywhere function worker_make_check(chunk)
-    cs = 0
-    for pair in chunk
-        cs += make_check(pair)
-    end
-    cs
-end
-
-@everywhere function run_benchmark(max_depth)
+function run_benchmark(max_depth)
     min_depth = 4
     max_depth = min_depth + 2 > max_depth ? min_depth + 2 : max_depth
 
